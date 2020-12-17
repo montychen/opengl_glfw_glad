c19d0c56c34988b41b9118050e0d7105e0a2d8d9
dajun <rcdchen@qq.com>
2020-12-17 18:39:11 +0800

    添加注释

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
     // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "大军的东东LearnOpenGL", NULL, NULL);
     if (window == NULL)
-        {
+    {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -57,6 +57,7 @@ int main()
         return -1;
     }
 
+    glfwSwapInterval(1);  // 默认交换间隔为0, 则在调用glfwSwapBuffers时立即进行交换,而不等待屏幕刷新的到来。如果设成1，那就表示Vsync屏幕刷新1次时，才交换缓冲区。
 
     // build and compile our shader program
     // ------------------------------------
@@ -100,31 +101,39 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+    // 1： 首先，我们先要定义（不重复的）顶点
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
+    0.5f, 0.5f, 0.0f,   // 右上角 0
+    0.5f, -0.5f, 0.0f,  // 右下角 1
+    -0.5f, -0.5f, 0.0f, // 左下角 2
+    -0.5f, 0.5f, 0.0f   // 左上角 3
+};
+
+// 2：定义绘制出矩形所需的索引：
+    unsigned int indices[] = { // 注意索引从0开始!
+    0, 1, 3, // 第一个三角形
+    1, 2, 3  // 第二个三角形
+};
+
+
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(VAO); // 绑定VAO, 执行VAO绑定之后其后的所有VBO配置、以及EBO配置都是这个VAO对象的一部分
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 把顶点数组复制到一个顶点缓冲中，供OpenGL使用
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // 绑定缓冲类型：顶点缓冲对象的类型是GL_ARRAY_BUFFER。也可以理解成，这里才是真正把缓冲定义为VBO类型，上面创建缓冲区时，并没有指定类型。
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  //一次性把CPU中定义的顶点数据传到GPU的VBO。 CPU => GPU
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    // 复制索引数组到一个索引缓冲中，glDrawElements函数从当前绑定到GL_ELEMENT_ARRAY_BUFFER目标的EBO中获取索引。
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);  // 把缓冲的类型定义为GL_ELEMENT_ARRAY_BUFFER，也就是EBO
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);  // 把索引复制到缓冲里：一次性把CPU中定义的索引数据传输到GPU的EBO里。 CPU => GPU
+
+    // 设定顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 一个VBO对应一个顶点属性，这里把两者关联对应起来。
+    glEnableVertexAttribArray(0); // 以顶点属性位置值为参数layout (location = 0) ，启用顶点属性；顶点属性默认是禁用的。
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -156,8 +165,9 @@ int main()
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 触发绘制开始执行
+        //glDrawArrays(GL_TRIANGLES, 0, 6);  // 当用EBO绑定顶点索引的方式绘制模型时，需要使用glDrawElements而不是glDrawArrays
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 触发绘制开始执行,  glDrawElements函数从当前绑定到GL_ELEMENT_ARRAY_BUFFER目标的EBO中获取索引。
+
         // glBindVertexArray(0); // no need to unbind it every time
 
         // GLFW窗口默认是双缓冲的，这意味着每个窗口有两个渲染缓冲区。 前缓冲区是正在显示的缓冲区，后缓冲区是即将显示的缓冲区。交换缓冲区的意思就是，前缓冲区变成后缓冲区，后缓冲区变成前缓冲区
